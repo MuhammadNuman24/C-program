0aac5340a588db34d1643061549915e9042cd656
Muhammad Numan <67092721+MuhammadNuman24@users.noreply.github.com>
2021-11-01 11:21:46 +0500

    Sum of twom variables in C

---
+++ Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+printf("My name is Numan\n");
+int num1 , num2, sum;
+printf("Enter value on a:");
+scanf("%d",&num1);
+printf("Enter valuwe of b:");
+scanf("%d",&num2);
+sum=num1+num2;
+printf("sum : %d\n", sum);
+return 0;
+}
